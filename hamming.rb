6fc642fc171fc98a1240865e605208adcc8ff1bd
Sarah Kirk <kirkse710@gmail.com>
2018-03-27 13:49:11 -0700

    first exercise

---
+++ hamming.rb
@@ -0,0 +1,16 @@
+# Examples
+# GAGCCTACTAACGGGAT
+# CATCGTAATGACGGCCT
+# ^ ^ ^  ^ ^    ^^
+#The hamming distance here is 7 because there are 7 places where there is a
+#difference between the first strand of DNA and the second
+
+#Hamming can only happen with DNA strands of equal length
+
+def equal_length(strand_one, strand_two)
+  if strand_one.length == strand_two.equal_length
+    true
+  else
+    false
+  end
+end
